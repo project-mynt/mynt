b669c67d89d86082143e5abf8d84d8a13cfc7b96
joenilan <joenilan@gmail.com>
2019-02-14 02:16:54 -0500

    update founders address

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -201,7 +201,7 @@ public:
         vector<FounderRewardStructure> rewardStructures = {  {10057440, 5}//, 5% founder/dev fee for blocks between startFounder block and 500k block
                                                              // {1420000, 5} 5% founder/dev fee for blocks between startFounder block and 500k block
                                                                            };
-        consensus.nFounderPayment = FounderPayment(rewardStructures, 2, "MVT2AJDK7CYTtWo5fX9u48eQT5ynWPyFFd");
+        consensus.nFounderPayment = FounderPayment(rewardStructures, 2, "MFgR3UCy3zVDzH1uSYQ7wRPnmoRJz9SQdh");
         assetStartBlock = 0;
 
 
@@ -455,7 +455,7 @@ public:
         vector<FounderRewardStructure> rewardStructures = {  {10057440, 5}//, 5% founder/dev fee for blocks between startFounder block and 500k block
                                                              // {1420000, 5} 5% founder/dev fee for blocks between startFounder block and 500k block
                                                                            };
-        consensus.nFounderPayment = FounderPayment(rewardStructures, 2, "MVT2AJDK7CYTtWo5fX9u48eQT5ynWPyFFd");
+        consensus.nFounderPayment = FounderPayment(rewardStructures, 2, "MFgR3UCy3zVDzH1uSYQ7wRPnmoRJz9SQdh");
         assetStartBlock = 100;
 
 
